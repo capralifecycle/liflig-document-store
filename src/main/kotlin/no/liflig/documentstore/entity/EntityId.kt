f670e0eb3e0e4b653516059d5cc6a600b7368f77
hermannm <hermann.morkrid@gmail.com>
2024-05-06 15:19:46 +0200

    Remove commented-out code

--- src/main/kotlin/no/liflig/documentstore/entity/EntityId.kt
+++ src/main/kotlin/no/liflig/documentstore/entity/EntityId.kt
@@ -14,23 +14,6 @@ interface UuidEntityId : EntityId {
   val value: UUID
 }
 
-// TODO: this is probably not possible to achieve with the serializationadapter?
-// /**
-// * Abstract class to simplify creating serializers for [UuidEntityId] implementations.
-// */
-// abstract class UuidEntityIdSerializer<T : UuidEntityId>(
-//  val factory: (UUID) -> T
-// ) : KSerializer<T> {
-//  override val descriptor: SerialDescriptor =
-//    PrimitiveSerialDescriptor("UuidEntityId", PrimitiveKind.STRING)
-//
-//  override fun serialize(encoder: Encoder, value: T) =
-//    encoder.encodeString(value.id.toString())
-//
-//  override fun deserialize(decoder: Decoder): T =
-//    factory(UUID.fromString(decoder.decodeString()))
-// }
-
 /** An argument factory for JDBI so that we can use a [UuidEntityId] as a bind argument. */
 class UuidEntityIdArgumentFactory : AbstractArgumentFactory<UuidEntityId>(Types.OTHER) {
   override fun build(value: UuidEntityId, config: ConfigRegistry?): Argument =
