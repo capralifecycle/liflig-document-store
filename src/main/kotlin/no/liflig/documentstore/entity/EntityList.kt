b8ae578f5e415bc88c5d1cb39d10f14a5a95e7d7
hermannm <hermann.morkrid@gmail.com>
2024-05-10 12:43:41 +0200

    Simplify implementations of EntityList utility functions

--- src/main/kotlin/no/liflig/documentstore/entity/EntityList.kt
+++ src/main/kotlin/no/liflig/documentstore/entity/EntityList.kt
@@ -8,41 +8,25 @@ typealias EntityList<T> = List<VersionedEntity<T>>
  * Utility function for mapping a list of entities, but keeping the same version for each element.
  */
 fun <T : EntityRoot<*>> EntityList<T>.mapEntities(mapFn: (T) -> T): EntityList<T> {
-  // Initialize new list with capacity equal to previous list's size, to avoid extra allocations
-  val newList = ArrayList<VersionedEntity<T>>(this.size)
-  for ((entity, version) in this) {
-    newList.add(VersionedEntity(mapFn(entity), version))
+  return map { (entity, version) ->
+    val newEntity = mapFn(entity)
+    VersionedEntity(newEntity, version)
   }
-  return newList
 }
 
 /** Same as [mapEntities], but discards entities that return null from the map function. */
 fun <T : EntityRoot<*>> EntityList<T>.mapEntitiesNotNull(mapFn: (T) -> T?): EntityList<T> {
-  // Not setting initial capacity here, since new list may be smaller than previous
-  val newList = ArrayList<VersionedEntity<T>>()
-  for ((entity, version) in this) {
-    val mapped = mapFn(entity)
-    if (mapped == null) {
-      continue
-    } else {
-      newList.add(VersionedEntity(mapped, version))
-    }
+  return mapNotNull { (entity, version) ->
+    val newEntity = mapFn(entity) ?: return@mapNotNull null
+    VersionedEntity(newEntity, version)
   }
-  return newList
 }
 
 /**
  * Utility function for filtering a list of entities, but keeping the same version for each element.
  */
 fun <T : EntityRoot<*>> EntityList<T>.filterEntities(predicate: (T) -> Boolean): EntityList<T> {
-  // Not setting initial capacity here, since new list may be smaller than previous
-  val newList = ArrayList<VersionedEntity<T>>()
-  for (entity in this) {
-    if (predicate(entity.item)) {
-      newList.add(entity)
-    }
-  }
-  return newList
+  return filter { (entity, _) -> predicate(entity) }
 }
 
 typealias EntityListWithTotalCount<T> = ListWithTotalCount<VersionedEntity<T>>
