c6c96ea2976013cd06b1fab594a029be01f5ee48
hermannm <hermann.morkrid@gmail.com>
2025-05-21 13:03:00 +0200

    Add TransactionManager class as an alternative to passing around Jdbi

--- src/main/kotlin/no/liflig/documentstore/repository/Transactions.kt
+++ src/main/kotlin/no/liflig/documentstore/repository/Transactions.kt
@@ -101,3 +101,78 @@ inline fun <ReturnT> transactional(jdbi: Jdbi, block: () -> ReturnT): ReturnT {
     }
   }
 }
+
+/**
+ * Utility class that wraps a [Jdbi] instance to provide a [transactional] method for running
+ * database transactions.
+ *
+ * This is useful when you want to use the top-level
+ * [transactional][no.liflig.documentstore.repository.transactional] function, but you don't want to
+ * pass around a `Jdbi` instance everywhere.
+ *
+ * If you're in the context of a repository, you should probably use [RepositoryJdbi.transactional]
+ * instead. But if you're running a transaction that includes multiple repositories, you may want to
+ * use this class instead to make it explicit that the transaction is not tied to a specific
+ * repository.
+ */
+open class TransactionManager(
+    @PublishedApi internal val jdbi: Jdbi,
+) {
+  /**
+   * Starts a database transaction, and runs the given [block] inside of it. Calls to [useHandle]
+   * inside the block will use the same transaction (this includes the various methods on
+   * [RepositoryJdbi]). If an exception is thrown, the transaction is rolled back.
+   *
+   * The repository's [Jdbi] instance is used for the transaction. If a transaction is already in
+   * progress on the current thread, a new one will not be started (since we're already in a
+   * transaction).
+   *
+   * ### Thread safety
+   *
+   * This function stores a transaction handle in a thread-local, so that operations within [block]
+   * can get the handle. But new threads spawned in the scope of `block` will not see this
+   * thread-local, and so they will not work correctly with the transaction. So you should not
+   * attempt concurrent database operations with this function.
+   *
+   * ### Mocking
+   *
+   * See [shouldMockTransactions].
+   */
+  inline fun <ReturnT> transactional(block: () -> ReturnT): ReturnT {
+    if (shouldMockTransactions()) {
+      return block()
+    }
+
+    return transactional(jdbi, block)
+  }
+
+  /**
+   * Since [transactional] is an inline method, it cannot be mocked by libraries such as
+   * [mockk](https://mockk.io/) that operate on bytecode (since inline functions don't generate
+   * bytecode). We still want `transactional` to be inline, to allow non-local returns in the lambda
+   * passed to it, which is handy for such scope-based functions.
+   *
+   * So to support users who want to mock calls to `transactional`, we provide this method, which
+   * users can override/mock to return true. When this returns true, `transactional` will just
+   * immediately call the lambda passed to it, without starting a database transaction.
+   *
+   * ### Example
+   *
+   * ```
+   * val mockTransactionManager =
+   *     mockk<TransactionManager> { every { shouldMockTransactions() } returns true }
+   *
+   * val mockRepo =
+   *     mockk<ExampleRepository> {
+   *       every { getOrThrow(id = any(), forUpdate = any()) } returns mockEntity
+   *       every { update(entity = any(), previousVersion = any()) } returns mockEntity
+   *     }
+   *
+   * mockTransactionManager.transactional {
+   *   val entity = mockRepo.getOrThrow(mockEntity.item.id, forUpdate = true)
+   *   mockRepo.update(entity.item, entity.version)
+   * }
+   * ```
+   */
+  open fun shouldMockTransactions(): Boolean = false
+}
